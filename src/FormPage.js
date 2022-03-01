5efde157351fc1860668e5b43ce01a746e054fc9
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2022-03-01 23:42:13 +0400

    created personal info

--- src/FormPage.js
+++ src/FormPage.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PersonalInformation from "./form-contents/PersonalInformation";
 import { data } from './data';
 const FormPage = () => {
+
+    const [index, setIndex] = useState(0);
+    const { id, questionName, descriptionName, description } = data[index];
+
+
 
     return (
         <div className="container">
             <section className="questionaire">
                 <div className="container-inner">
-                    <h1>{data[0].quetionName}</h1>
-
+                    <h1>{questionName}</h1>
+                    <form action="">
+                        <PersonalInformation />
+                    </form>
                 </div>
             </section>
             <section className="description">
--- src/form-contents/PersonalInformation.js
+++ src/form-contents/PersonalInformation.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PersonalInformation = () => {
 
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumer] = useState('');
+
+    const setPhone = () => {
+        if (phoneNumber[0] === '+' && phoneNumber.length === 13) {
+            let ph = '';
+            for (let i = 0; i < phoneNumber.length; i++) {
+                ph += phoneNumber[i];
+                if (i > 0) {
+                    if (i === 3) ph += ' ';
+                    if (i === 6) ph += ' ';
+                    if (i === 8) ph += ' ';
+                    if (i === 10) ph += ' ';
+                    if (i === 12) ph += ' ';
+                    setPhoneNumer(ph);
+                }
+            }
+        }
+
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', setPhone);
+        return () => {
+            document.removeEventListener('click', setPhone);
+        }
+    }, [setPhone]);
+
     return (
-        <></>
+        <div className="form-app">
+            <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder='First Name' required />
+            <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} placeholder='Last Name' required />
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder='E Mail' required />
+            <input type="tel" maxLength='18' value={phoneNumber} onChange={e => setPhoneNumer(e.target.value)} placeholder='+995 5__ __ __ __' pattern='[+]{1}[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2} [0-9]{2}' required />
+        </div>
     );
 }
 
