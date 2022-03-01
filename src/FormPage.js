324f997a97f26cbb487de517358bfc1d9d391f42
Ioane-stacks <ioane.natroshvili08b@geolab.edu.ge>
2022-03-01 21:05:13 +0400

    added data.js and import data dinamically

--- src/FormPage.js
+++ src/FormPage.js
@@ -1,15 +1,18 @@
 import React from "react";
-
+import PersonalInformation from "./form-contents/PersonalInformation";
+import { data } from './data';
 const FormPage = () => {
 
     return (
         <div className="container">
             <section className="questionaire">
                 <div className="container-inner">
-                    <h1>Hey, Rocketeer, what are your coordinates?</h1>
+                    <h1>{data[0].quetionName}</h1>
+
                 </div>
             </section>
-            <section className="description"></section>
+            <section className="description">
+            </section>
         </div>
     );
 }
